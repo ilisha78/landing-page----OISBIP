0e04573c23a8b34e08ca749aa52a8827b542946a
ilisha <jaiswalilisha@gmail.com>
2023-12-01 19:34:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+about = () => {
+    var aboutPopup = document.getElementById("abt");
+    aboutPopup.style.display =
+    aboutPopup.style.display === "none" ? "block" : "none";
+    // console.log("bhumi", aboutPopup.style.display);s
+};
+
+features = () => {
+    var featuresPopup = document.getElementById("feat");
+    featuresPopup.style.display =
+    featuresPopup.style.display === "none" ? "block" : "none";
+    // console.log("bhumi", featuresPopup.style);
+};
+
+explore = () => {
+    var explorePopup = document.getElementById("expl");
+    explorePopup.style.display =
+    explorePopup.style.display === "block" ? "none" : "block";
+    // console.log("bhumi", explorePopup.style);
+};
+
+register = () => {
+    var registerPopup = document.getElementById("pop-up");
+    registerPopup.style.display =
+    registerPopup.style.display === "block" ? "none" : "block";
+    // console.log("bhumi", registerPopup.style);
+};
